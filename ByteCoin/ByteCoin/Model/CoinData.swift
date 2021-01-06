27e1a579170f81b0d5120f695ae49501d11a4d05
sharafa839 <77029272+sharafa839@users.noreply.github.com>
2021-01-05 16:32:02 -0800

    Add files via upload

---
+++ ByteCoin/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by sharaf  on 25/12/2020.
+//  Copyright © 2020 sharaf. All rights reserved.
+// 
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
